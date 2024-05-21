533759b454182646dee3679bd7c309074f87a0d3
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-21 23:56:23 +0530

    wrote a passing  test - should render a button (not null)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
     <div className="bg-gray-900 text-gray-200 h-screen">
       <div className="container mx-auto">
         <div data-testid = "gameBoard" className="flex justify-center items-center h-screen">
-          X
+          <button>X</button>
         </div>
       </div>
     </div>
