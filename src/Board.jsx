850fe49e682c9f3e967426d99c92c7d6217c2e75
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-22 01:29:20 +0530

    added flex to each cell

--- src/Board.jsx
+++ src/Board.jsx
@@ -10,17 +10,17 @@ export default function Board() {
     }
     return (
         <div className="flex flex-col">
-            <div>
+            <div className="flex">
               <Square value = {squares[0]} onSquareClick = {() => handleClick(0)}/>
               <Square value = {squares[1]} onSquareClick = {() => handleClick(1)}/>
               <Square value = {squares[2]} onSquareClick = {() => handleClick(2)}/>
             </div>
-            <div>
+            <div className="flex">
               <Square value = {squares[3]} onSquareClick = {() => handleClick(3)} />
               <Square value = {squares[4]} onSquareClick = {() => handleClick(4)} />
               <Square value = {squares[5]} onSquareClick = {() => handleClick(5)} />
             </div>
-            <div>
+            <div className="flex">
               <Square value = {squares[6]} onSquareClick = {() => handleClick(6)} />
               <Square value = {squares[7]} onSquareClick = {() => handleClick(7)} />
               <Square value = {squares[8]} onSquareClick = {() => handleClick(8)} />
