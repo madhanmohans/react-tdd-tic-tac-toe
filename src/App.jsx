ff66e2905b0260a6aa0d0deab98a024814bdb2f3
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-22 00:01:27 +0530

    added style for button (refactor)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
     <div className="bg-gray-900 text-gray-200 h-screen">
       <div className="container mx-auto">
         <div data-testid = "gameBoard" className="flex justify-center items-center h-screen">
-          <button>X</button>
+          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
         </div>
       </div>
     </div>
