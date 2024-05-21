025929f588152ba7e0c4a1c29ebfe3676d7aed6d
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-22 00:18:50 +0530

    made 3x3 squares (refactor)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import Square from "./Square"
 export default function App() {
   return(
     <div className="bg-gray-900 text-gray-200 h-screen">
       <div className="container mx-auto">
         <div data-testid = "gameBoard" className="flex justify-center items-center h-screen">
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
-          <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
+          <div className="flex flex-col">
+            <div>
+              <Square />
+              <Square />
+              <Square />
+            </div>
+            <div>
+              <Square />
+              <Square />
+              <Square />
+            </div>
+            <div>
+              <Square />
+              <Square />
+              <Square />
+            </div>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/Square.jsx
@@ -0,0 +1,5 @@
+export default function Square() {
+    return (
+        <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
+    );
+}
