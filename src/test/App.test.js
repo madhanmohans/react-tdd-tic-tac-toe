c05b9ead2e0e81bf1e702e449d89d83a62ddc5c6
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-21 23:08:41 +0530

    wrote the 1st failing test - should display X

---
+++ src/test/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("App", () => {
+    it("should display X", () => {
+        render(<App />);
+        const gameBoard = screen.getByTestId("gameBoard");
+        expect(gameBoard).not.toBeNull();
+    });
+});
