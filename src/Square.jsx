74a68b7769e42e7af562710cd679a25046eafd01
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-22 00:25:01 +0530

    added 1-9 values to squares

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,5 +1,5 @@
-export default function Square() {
+export default function Square( {value} ) {
     return (
-        <button className="border w-10 h-10 font-bold font-mono text-3xl">X</button>
+        <button className="border w-10 h-10 font-bold font-mono text-3xl">{value}</button>
     );
 }
